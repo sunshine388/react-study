3e71fe4b1395a75e07c15210602b9ac44df1abcf
熄月 <2723166086@qq.com>
2022-06-21 09:30:56 +0800

    feat：openlayers—弹窗

--- src/router/modules/openlayers.js
+++ src/router/modules/openlayers.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import loadable from '@/utils/loadable';
 
 const StatedView = loadable(() => import('@/views/OpenLayers/Stated'));
+const PopupView = loadable(() => import('@/views/OpenLayers/Popup'));
 
 export const openlayersRoutes = [
-  { path: 'openlayers/stated', component: <StatedView /> }
+  { path: 'openlayers/stated', component: <StatedView /> },
+  { path: 'openlayers/popup', component: <PopupView /> },
 ];
 
 export const openlayersMenu = {
@@ -16,6 +18,10 @@ export const openlayersMenu = {
     {
       label: <Link to='/openlayers/stated'>起步</Link>,
       key: '/openlayers/stated'
+    },
+    {
+      label: <Link to='/openlayers/popup'>弹窗 popup</Link>,
+      key: '/openlayers/popup'
     }
   ]
 };
