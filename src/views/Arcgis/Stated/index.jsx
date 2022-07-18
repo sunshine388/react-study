4c27a9cb3570acdcd81d0133aa076b9257533ab5
熄月 <2723166086@qq.com>
2022-07-18 09:47:43 +0800

    fix：修改arcgis，leaflet起步

--- src/views/Arcgis/Stated/index.jsx
+++ src/views/Arcgis/Stated/index.jsx
@@ -5,11 +5,10 @@ import { loadModules } from 'esri-loader';
 export default function StatedView() {
   const initMap = () => {
     loadModules(['esri/Map', 'esri/views/MapView']).then(([Map, MapView]) => {
-      this.esriMap = new Map({
-        basemap: 'streets-vector'
-      });
       new MapView({
-        map: this.esriMap,
+        map: new Map({
+          basemap: 'streets-vector'
+        }),
         container: 'map',
         center: [-118.244, 34.052],
         zoom: 12
--- src/views/Leaflet/Stated/index.jsx
+++ src/views/Leaflet/Stated/index.jsx
@@ -7,10 +7,19 @@ export default function StatedView() {
     let mymap = L.map('map').setView([51.505, -0.09], 13);
 
     // eslint-disable-next-line
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap'
-    }).addTo(mymap);
+    L.tileLayer(
+      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3Vuc2hpbmUzODgiLCJhIjoiY2w1bTkwYzl2MHJ0cjNncWlvMzA5aGYzeSJ9.uaM3WZCNU2_Cg2yXBi-ilA',
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          'pk.eyJ1Ijoic3Vuc2hpbmUzODgiLCJhIjoiY2w1bTkwYzl2MHJ0cjNncWlvMzA5aGYzeSJ9.uaM3WZCNU2_Cg2yXBi-ilA'
+      }
+    ).addTo(mymap);
   };
 
   useEffect(() => {
